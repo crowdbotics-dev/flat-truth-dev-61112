a915917106e4d421ba41a07c0793fc1b9d5fa28f
Crowdbotics <engineering@crowdbotics.com>
2023-02-04 00:25:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-truth-dev-61112",
+  url: "https://flat-truth-dev-61112.botics.co",
+  api: "https://flat-truth-dev-61112.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
